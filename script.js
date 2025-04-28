194c2138baae76783a90062e8087ba8f4e00f4d3
srimoorthy <srimoorthy27@gmail.com>
2025-04-28 18:16:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let inp = document.getElementById("evaluatedText");
+let cnt = document.getElementById("letterCount");
+
+inp.addEventListener("input",function(){
+	cnt.textContent = inp.value.length;
+});
